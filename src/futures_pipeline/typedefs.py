1686a6ebbb3d747b45ab1a45f4c736acbce89dff
Matt Warner <mattwarner744@gmail.com>
2026-09-10 13:50:58 -0500

    Wrote BacktestResults class

--- src/futures_pipeline/typedefs.py
+++ src/futures_pipeline/typedefs.py
@@ -1,7 +1,19 @@
 from dataclasses import dataclass
 from typing import Required, TypedDict
 from pandas import Series, DataFrame
-from .validate import CommandArgs, FetchLatestArgs, FetchRangeArgs, FetchLookbackArgs, ModelArgs, PreprocessArgs, TradingFees
+from enum import IntEnum
+from .validate import (
+    CommandArgs,
+    FetchLatestArgs,
+    FetchRangeArgs,
+    FetchLookbackArgs,
+    ModelArgs,
+    PreprocessArgs,
+    TradingFees,
+    TradingFeeUpdates,
+    ContractSpec,
+    PredictionInterval
+)
 from typing import Literal
 import re
 
@@ -41,6 +53,25 @@ class EvaluationResult:
     quantile_calibration: dict[float, float]
     calibration_error: dict[float, float]
 
+@dataclass
+class BacktestResults:
+    trade_count: int
+    net_pnl: float
+    win_rate: float
+    average_pnl: float
+    gross_profit: float
+    gross_loss: float
+
+    def display_results(self) -> None:
+        print(f"Trade count:   {self.trade_count:,}")
+        print(f"Net P&L:       ${self.net_pnl:,.2f}")
+        print(f"Win rate:      {self.win_rate:.2%}")
+        print(f"Average P&L:   ${self.average_pnl:,.2f}")
+        print(f"Gross profit:  ${self.gross_profit:,.2f}")
+        print(f"Gross loss:    ${self.gross_loss:,.2f}")
+
+
+
 type InputArgs = (
         CommandArgs 
         | FetchLookbackArgs 
@@ -48,7 +79,7 @@ type InputArgs = (
         | FetchLatestArgs 
         | ModelArgs 
         | PreprocessArgs
-        | TradingFees
+        | TradingFeeUpdates
 )
 
 type TimedeltaUnit = Literal["s", "min", "h", "D", "W"]
@@ -81,3 +112,8 @@ class CandleResolution:
     def to_timedelta_unit(self) -> TimedeltaUnit:
         return self.__fixed_units[self.unit]
 
+
+class Signal(IntEnum):
+    LONG = 1
+    SHORT = -1
+    HOLD = 0
